f74d24825f71f27c15042078221ab9ec2e7a0523
weihang <pehweihang@live.com>
2023-10-06 05:54:45 +0800

    added mini-animate

---
+++ lua/weihang/plugins/mini-animate.lua
@@ -0,0 +1,35 @@
+return {
+  "echasnovski/mini.animate",
+  config = function()
+    local animate = require("mini.animate")
+    animate.setup({
+      -- Cursor path
+      cursor = {
+        enable = true,
+        timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+      },
+
+      -- Vertical scroll
+      scroll = {
+        enable = true,
+        timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+      },
+
+      -- Window resize
+      resize = {
+        enable = true,
+        timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+      },
+
+      open = {
+        enable = true,
+        timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+      },
+
+      close = {
+        enable = true,
+        timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+      },
+    })
+  end,
+}
