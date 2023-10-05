6d03c7a74516a7213f0241bb1541b66f315aa590
weihang <pehweihang@live.com>
2023-10-06 05:56:58 +0800

    update nerd font icon

--- lua/weihang/plugins/gitsigns.lua
+++ lua/weihang/plugins/gitsigns.lua
@@ -13,13 +13,13 @@ return {
 				},
 				delete = {
 					hl = "GitSignsDelete",
-					text = "契",
+					text = "󰐊",
 					numhl = "GitSignsDeleteNr",
 					linehl = "GitSignsDeleteLn",
 				},
 				topdelete = {
 					hl = "GitSignsDelete",
-					text = "契",
+					text = "󰐊",
 					numhl = "GitSignsDeleteNr",
 					linehl = "GitSignsDeleteLn",
 				},
