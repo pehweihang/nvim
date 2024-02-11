819fceea92545e7dd1e965ce312ee2a5eaef4e45
pehweihang <pehweihang@live.com>
2024-02-12 05:43:41 +0800

    lsp stuff

--- lua/weihang/plugins/lsp.lua
+++ lua/weihang/plugins/lsp.lua
@@ -25,10 +25,50 @@ return {
 			},
 		})
 
+		local signs = {
+			{ name = "DiagnosticSignError", text = "" },
+			{ name = "DiagnosticSignWarn", text = "" },
+			{ name = "DiagnosticSignHint", text = "󰌵" },
+			{ name = "DiagnosticSignInfo", text = "" },
+		}
+
+		for _, sign in ipairs(signs) do
+			vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+		end
+
+		vim.diagnostic.config({
+			virtual_text = {
+				prefix = "● ",
+				source = "if_many",
+			},
+			signs = {
+				active = signs,
+			},
+			update_in_insert = true,
+			underline = true,
+			severity_sort = true,
+			float = {
+				focusable = true,
+				style = "minimal",
+				border = "rounded",
+				source = "always",
+				header = "",
+				prefix = "",
+			},
+		})
+
+		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+			border = "rounded",
+		})
+
+		vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+			border = "rounded",
+		})
+
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-		local on_attach = function(client, bufnr)
+		local on_attach = function(_, bufnr)
 			local opts = { noremap = true, silent = true }
 			local keymap = vim.api.nvim_buf_set_keymap
 			keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
@@ -43,22 +83,22 @@ return {
 			vim.api.nvim_buf_create_user_command(bufnr, "Format", "lua vim.lsp.buf.format()", {})
 		end
 
+		local servers_settings = {
+			lua_ls = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+				},
+			},
+		}
+
 		require("mason-lspconfig").setup_handlers({
 			function(server_name)
 				require("lspconfig")[server_name].setup({
 					capabilities = capabilities,
 					on_attach = on_attach,
-				})
-			end,
-			lua_ls = function()
-				require("lspconfig")["lua_ls"].setup({
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" },
-							},
-						},
-					},
+					settings = servers_settings[server_name],
 				})
 			end,
 		})
