7b641c79d2eb4b383b2a4552157cf186fcabf9a5
Peh Wei Hang <pehweihang@live.com>
2024-07-10 22:49:08 +0800

    latte

--- lua/weihang/plugins/catppuccin.lua
+++ lua/weihang/plugins/catppuccin.lua
@@ -4,7 +4,7 @@ return {
 	priority = 1000,
 	config = function()
 		require("catppuccin").setup({
-			flavour = "mocha",
+			flavour = "latte",
 			background = {
 				light = "latte",
 				dark = "mocha",
