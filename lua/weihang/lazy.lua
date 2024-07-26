97fab314a79acd6c8c79588226dedf791c9f122e
Peh Wei Hang <pehweihang@live.com>
2024-07-27 00:48:32 +0800

    formatting

--- lua/weihang/lazy.lua
+++ lua/weihang/lazy.lua
@@ -1,20 +1,23 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Use a protected call so we don't error out on first use
 local status_ok, lazy = pcall(require, "lazy")
 if not status_ok then
-  return
+	return
 end
 
-lazy.setup({ import = "weihang.plugins" }, { ui = { border = "rounded" }, change_detection = { notify = false } })
+lazy.setup({
+	spec = { import = "weihang.plugins" },
+	{ ui = { border = "rounded" }, change_detection = { notify = false } },
+})
