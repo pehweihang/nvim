40690e19713c74c882e595c8803f69fcbe5a509a
Peh Wei Hang <pehweihang@live.com>
2024-08-05 23:34:39 +0800

    update and fix config

--- lua/weihang/lazy.lua
+++ lua/weihang/lazy.lua
@@ -1,13 +1,16 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -19,6 +22,7 @@ end
 
 lazy.setup({
 	spec = { import = "weihang.plugins" },
-	{ ui = { border = "rounded" }, change_detection = { notify = false } },
-	concurrency = 1,
+	ui = { border = "rounded" },
+	change_detection = { notify = false },
+	concurrency = 3,
 })
