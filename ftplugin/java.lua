b8eedfe4706b2279e00c85f899e1591ec6131eff
Peh Wei Hang <weihang@pehweihang.com>
2025-06-03 00:00:18 +0800

    remove on_attach

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -3,7 +3,6 @@ local jdtls_bin = vim.fn.stdpath("data") .. "/mason/bin/jdtls"
 local config = {
 	cmd = { jdtls_bin },
 	root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
-	on_attach = require("weihang.plugins.lsp").on_attach,
 }
 
 require("jdtls").start_or_attach(config)
--- lua/weihang/plugins/rustaceannvim.lua
+++ lua/weihang/plugins/rustaceannvim.lua
@@ -5,7 +5,6 @@ return {
 	config = function()
 		vim.g.rustaceanvim = {
 			server = {
-				on_attach = require("weihang.plugins.lsp").on_attach,
 				capabilities = require("weihang.plugins.lsp").capabilities,
 			},
 		}
