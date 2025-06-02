d4f1fd04f6bf9b5cc42863a40965f3a723ae9cf5
Peh Wei Hang <weihang@pehweihang.com>
2025-06-02 23:07:10 +0800

    fix deprecated signs config

--- lua/weihang/plugins/lsp.lua
+++ lua/weihang/plugins/lsp.lua
@@ -44,24 +44,18 @@ M.config = function()
 		},
 	})
 
-	local signs = {
-		{ name = "DiagnosticSignError", text = "" },
-		{ name = "DiagnosticSignWarn", text = "" },
-		{ name = "DiagnosticSignHint", text = "󰌵" },
-		{ name = "DiagnosticSignInfo", text = "" },
-	}
-
-	for _, sign in ipairs(signs) do
-		vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-	end
-
 	vim.diagnostic.config({
 		virtual_text = {
 			prefix = "● ",
 			source = "if_many",
 		},
 		signs = {
-			active = signs,
+			text = {
+				[vim.diagnostic.severity.ERROR] = "",
+				[vim.diagnostic.severity.WARN] = "",
+				[vim.diagnostic.severity.HINT] = "󰌵",
+				[vim.diagnostic.severity.INFO] = "",
+			},
 		},
 		update_in_insert = false,
 		underline = true,
