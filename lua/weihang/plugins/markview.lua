1a4a49cd42e6a78978f4a68fd53f9e64b85ce91a
pehweihang <pehweihang@live.com>
2025-06-26 21:56:54 +0800

    change back to render-markdown

--- lua/weihang/plugins/markview.lua
+++ lua/weihang/plugins/markview.lua
@@ -1,11 +1,28 @@
 return {
 	"OXY2DEV/markview.nvim",
+	enabled = false,
 	lazy = false,
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 		"echasnovski/mini.icons",
 	},
 	opts = {
+		preview = {
+			hybrid_modes = { "n" },
+			filetypes = {
+				"md",
+				"markdown",
+				"norg",
+				"rmd",
+				"org",
+				"vimwiki",
+				"typst",
+				"latex",
+				"quarto",
+				"Avante",
+				"codecompanion",
+			},
+		},
 		latex = { enable = false },
 	},
 }
--- lua/weihang/plugins/render-markdown.lua
+++ lua/weihang/plugins/render-markdown.lua
@@ -1,6 +1,20 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
-	enabled = false,
+	enabled = true,
 	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
-	opts = {},
+	opts = {
+		file_types = {
+			"md",
+			"markdown",
+			"norg",
+			"rmd",
+			"org",
+			"vimwiki",
+			"typst",
+			"latex",
+			"quarto",
+			"Avante",
+			"codecompanion",
+		},
+	},
 }
