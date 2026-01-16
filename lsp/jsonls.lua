cbbf8050b5fcad05e7a065227ba4510f3968cf9f
Peh Wei Hang <weihang@pehweihang.com>
2026-01-16 13:35:19 +0800

    fix structure

--- lsp/jsonls.lua
+++ lsp/jsonls.lua
@@ -1,6 +1,8 @@
 return {
-	json = {
-		schemas = require("schemastore").json.schemas(),
-		validate = { enable = true },
+	settings = {
+		json = {
+			schemas = require("schemastore").json.schemas(),
+			validate = { enable = true },
+		},
 	},
 }
