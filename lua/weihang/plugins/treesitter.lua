5ac812f23f1611d0f5b31b09fdfc59a268a47440
Peh Wei Hang <weihang@pehweihang.com>
2026-07-19 17:51:54 +0800

    Update config

--- lua/weihang/plugins/treesitter.lua
+++ lua/weihang/plugins/treesitter.lua
@@ -1,30 +1,26 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-
-			ensure_installed = {
-				"vimdoc",
-				"javascript",
-				"typescript",
-				"python",
-				"c",
-				"cpp",
-				"lua",
-				"rust",
-				"jsdoc",
-				"bash",
-			},
-			sync_install = false,
-			ignore_install = { "" }, -- List of parsers to ignore installing
-			auto_install = true,
-			highlight = {
-				enable = true, -- false will disable the whole extension
-				disable = { "" }, -- list of language that will be disabled
-				additional_vim_regex_highlighting = true,
-			},
-		})
-	end,
+	opts = {
+		ensure_installed = {
+			"vimdoc",
+			"javascript",
+			"typescript",
+			"python",
+			"c",
+			"cpp",
+			"lua",
+			"rust",
+			"jsdoc",
+			"bash",
+		},
+		sync_install = false,
+		ignore_install = { "" }, -- List of parsers to ignore installing
+		auto_install = true,
+		highlight = {
+			enable = true, -- false will disable the whole extension
+			disable = { "" }, -- list of language that will be disabled
+			additional_vim_regex_highlighting = true,
+		},
+	},
 }
